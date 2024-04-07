740e21a1ccbc43a5162db6055058476a5a5268c8
PnavTheMonkey <20321@wc.school.nz>
2024-04-08 10:10:38 +1200

    Creating the htmls and the data source

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ from flask import Flask, render_template, redirect, request
 import sqlite3
 from sqlite3 import Error
 
+DATABASE = 'C:/Users/mam-m/OneDrive - Wellington College/DTE 13/cafe_V2/smile.db'
+app = Flask(__name__)
+
+
+
 
 
