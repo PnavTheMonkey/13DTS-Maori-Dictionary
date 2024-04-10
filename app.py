25e24501e51cd1ff90dee2805dc82fc3ff794682
PnavTheMonkey <20321@wc.school.nz>
2024-04-11 09:16:57 +1200

    Creating connection for the import modules in flask and getting the title page to work

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request  #imports the neccesary modules from Flask
 import sqlite3
 from sqlite3 import Error
 
-DATABASE = 'C:/Users/mam-m/OneDrive - Wellington College/DTE 13/cafe_V2/smile.db'
+DATABASE ='./database.db'
 app = Flask(__name__)
 
 
+def create_connection(db_file):
+    try:
+        connection = sqlite3.Connection(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
+@app.route('/')
+def render_homepage():
+    return render_template('home.html')
+    print("ssss")
+
+
+
+app.run(host='0.0.0.0', debug=True)
 
 
